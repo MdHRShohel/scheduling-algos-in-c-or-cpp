10cac645e29a4c882394a7c4955f00133ef1d6d3
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-21 22:53:24 +0600

    Update String1.cpp

--- String1.cpp
+++ String1.cpp
@@ -11,3 +11,29 @@ int main()
     cout << "You entered: " << str << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declaring a string object
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "You entered: " << str << endl;
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Declaring a string object
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    cout << "You entered: " << str << endl;
+    return 0;
+}
