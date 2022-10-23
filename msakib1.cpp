e04352e237c72a8af5560a532970779fdc198ea8
msakib7 <63781419+msakib7@users.noreply.github.com>
2022-10-23 18:54:55 +0600

    Create msakib1.cpp

---
+++ msakib1.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   for(int i = 0; i <= 9; i++){
+     for(int j = 0; j <= i; j++){
+       cout<<"*";
+     }
+     cout<<endl;
+   }
+  
+ return 0; 
+}
